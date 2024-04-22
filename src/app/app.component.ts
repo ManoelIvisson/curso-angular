fd81b6b483f9df6b974de428d955b9058e8c688d
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-04-22 17:23:03 -0300

    Criando diretiva de estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { FisrtComponentComponent } from './components/fisrt-component/fisrt-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent} from './components/directives/directives.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FisrtComponentComponent],
+  imports: [RouterOutlet, FisrtComponentComponent, ParentDataComponent, DirectivesComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  userName = "Tomé";
+  userData = {
+    email: "tome@hotmail.com",
+    role: "Admin"
+  };
+
   title = 'curso-angular';
 }
---
+++ src/app/components/directives/directives.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-directives',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './directives.component.html',
+  styleUrl: './directives.component.css'
+})
+export class DirectivesComponent {
+  color: string = "red";
+
+  classes: Array<string> = ["Titulo-verde", "Titulo-Azul"];
+}
