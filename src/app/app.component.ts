f94d5e69c720a58eaacf50056c030863a4b2d909
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-04-23 14:29:29 -0300

    Usando ngIf e else em Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FisrtComponentComponent } from './components/fisrt-component/fisrt-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent} from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FisrtComponentComponent, ParentDataComponent, DirectivesComponent, CommonModule],
+  imports: [RouterOutlet, FisrtComponentComponent, ParentDataComponent, DirectivesComponent, CommonModule, IfRenderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
